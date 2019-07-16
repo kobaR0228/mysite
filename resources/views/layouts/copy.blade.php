2fd6bc5aadf11276b4b36f78eed26997bfe67c68
EC2 Default User <ec2-user@ip-172-31-47-181.ap-northeast-1.compute.internal>
2019-07-16 10:30:05 +0000

    first commit

---
+++ resources/views/layouts/copy.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{secure_asset('css/app.css')}}" />
+    <title>{{config('app.name', 'LSAPP')}}</title>
+    
+</head>
+<body>
+    @include('inc.navbar')
+    <h1></h1>
+   <div class="container">
+    @include('inc.messages')
+    @yield('content')
+    </div>
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
+</body>
+</html>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+    @section('content')
+        <h1>post</h1>
+        @if(count($posts)>0)
+            @foreach($posts as $post)
+                <div class="card">
+                  <div class="card-body">
+                    <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
+                    <small>Written on {{$post->created_at}}by {{$post->user->name}}</small>
+                  </div>
+                </div>
+            @endforeach
+            {{$posts->links()}}
+            
+        @else
+         <p>No posts found</p>
+        @endif
+    @endsection('content')
